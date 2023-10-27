4ebc2d0452c7585fd03df17e73f0aa8275543997
fangxuqi.turing <fangxuqi.turing@bytedance.com>
2023-10-27 15:37:51 +0800

    feat: update config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'localhost',
-    port: 5001,
+    host: '0.0.0.0',
+    port: 3000,
+    open: true,
   }
 })
