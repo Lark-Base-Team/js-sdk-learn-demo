5adeab7d147fa08c86cc8222686f775ee083260e
Lark-Base <141317638+Lark-Base@users.noreply.github.com>
2025-05-20 18:53:42 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', //请勿删除此语句，否则将导致部署失败
   plugins: [react()],
   server: {
     host: '0.0.0.0',
